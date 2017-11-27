423ddba7ca3b2c10d3f050895249cf9cebe341d9
suzhenhui <362680581@qq.com>
2017-11-27 16:22:54 +0800

    "修复button多余阴影"

--- app/src/main/java/com/magicsu/android/beatbox/fragment/BeatBoxFragment.java
+++ app/src/main/java/com/magicsu/android/beatbox/fragment/BeatBoxFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -74,6 +75,7 @@ public class BeatBoxFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
+            Log.d("1111", "onClick: 111");
             mBeatBox.play(mSound);
         }
     }
